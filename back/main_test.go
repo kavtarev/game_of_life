1b14d85b0e2d0568c7565c1e5cd92eb2c945ddac
ivan kavtarev <ivan.kavtarev@mail.ru>
2024-10-17 09:56:36 +0300

    more test

--- back/main_test.go
+++ back/main_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestMain(t *testing.T) {
+func TestMain3x3(t *testing.T) {
 	schemas := [][2]string{
 		{
 			`
@@ -95,5 +95,102 @@ func TestMain(t *testing.T) {
 		})
 
 	}
+}
 
+func TestMain4x4(t *testing.T) {
+	schemas := [][2]string{
+		{
+			`
+			1111
+			1100
+			1010
+			1001
+			`,
+			`
+			1010
+			0001
+			1010
+			0100
+			`,
+		},
+		{
+			`
+			1011
+			1100
+			0010
+			1101
+			`,
+			`
+			1010
+			1001
+			0010
+			0110
+			`,
+		},
+	}
+
+	for i := 0; i < len(schemas); i++ {
+		t.Run(fmt.Sprintf("test %v", i), func(t *testing.T) {
+			f := NewField(4)
+			reg := regexp.MustCompile(`\D`)
+			init := reg.ReplaceAllString(schemas[i][0], "")
+			f.update(init)
+			f.run()
+			if reg.ReplaceAllString(schemas[i][1], "") != f.getStateString() {
+				t.Fatal()
+			}
+		})
+
+	}
+}
+
+func TestMain5x5(t *testing.T) {
+	schemas := [][2]string{
+		{
+			`
+			10111
+			10100
+			11111
+			00101
+			11101
+			`,
+			`
+			00110
+			10000
+			10001
+			00001
+			01100
+			`,
+		},
+		{
+			`
+			00100
+			01010
+			10001
+			01010
+			00100
+			`,
+			`
+			00100
+			01110
+			11011
+			01110
+			00100
+			`,
+		},
+	}
+
+	for i := 0; i < len(schemas); i++ {
+		t.Run(fmt.Sprintf("test %v", i), func(t *testing.T) {
+			f := NewField(5)
+			reg := regexp.MustCompile(`\D`)
+			init := reg.ReplaceAllString(schemas[i][0], "")
+			f.update(init)
+			f.run()
+			if reg.ReplaceAllString(schemas[i][1], "") != f.getStateString() {
+				t.Fatal()
+			}
+		})
+
+	}
 }
