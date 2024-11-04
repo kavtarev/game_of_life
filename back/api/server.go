a2ecf4c8342fd6c319e234310fd4b0bf84763923
ivan kavtarev <ivan.kavtarev@mail.ru>
2024-11-04 22:18:56 +0300

    init grafana + prometheus

--- back/api/server.go
+++ back/api/server.go
@@ -12,6 +12,9 @@ import (
 	"game_of_life/internal"
 
 	"github.com/gorilla/websocket"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Message struct {
@@ -39,6 +42,27 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+type metrics struct {
+	gauge prometheus.Gauge
+	info  *prometheus.GaugeVec
+}
+
+func NewMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "game_of_life", Name: "default_gauge", Help: "default descriptor of gauge",
+		}),
+		info: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "game_of_life",
+			Name:      "info",
+			Help:      "Information about the My App environment.",
+		},
+			[]string{"version"}),
+	}
+	reg.MustRegister(m.gauge, m.info)
+	return m
+}
+
 func NewServer(port string, s *db.Storage) *Server {
 	return &Server{
 		port:    port,
@@ -50,9 +74,23 @@ func NewServer(port string, s *db.Storage) *Server {
 func (s *Server) Run() {
 	mux := http.NewServeMux()
 
+	reg := prometheus.NewRegistry()
+	m := NewMetrics(reg)
+	m.gauge.Set(2)
+	m.info.With(prometheus.Labels{"version": "1.0.0"}).Set(1)
+
 	mux.HandleFunc("/next", handlers.HandleComputeNextForm)
 	mux.HandleFunc("/ws", s.handleConnections)
 	mux.Handle("/", http.FileServer(http.Dir("../front")))
+
+	// Expose /metrics HTTP endpoint using the created custom registry.
+	mux.Handle(
+		"/metrics", promhttp.HandlerFor(
+			reg,
+			promhttp.HandlerOpts{
+				EnableOpenMetrics: true,
+			}),
+	)
 
 	http.ListenAndServe(s.port, mux)
 }
